66efca942a47f53f66f9c10d23802c2f0f1e5ea1
alexbcberio <alexbcberio@gmail.com>
2021-05-08 21:33:02 +0200

    :tada: Added project files

---
+++ DjangoWebProjectVS2017/env2/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!E:\Documentos\Escritorio\UNI\VS-projects\HADS\Django\DjangoWebProjectVS2017\env2\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
